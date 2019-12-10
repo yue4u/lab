f0826e51c3f3ce963af9dde2f778eaf6594e5b47
rainy-me <github@rainy.me>
2019-12-11 03:39:49 +0900

    add ng-galar-pokemons

---
+++ external/ng-galar-pokemons.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/rainy-me/ng-galar-pokemons.git
+cd ./ng-galar-pokemons
+
+yarn
+ng build --prod --deploy-url /ng-galar-pokemons/
+
+cd ..
+[ ! -d "./dist/ng-galar-pokemons" ] && mkdir ./dist/ng-galar-pokemons
+rm -rf ./dist/ng-galar-pokemons/*
+cp -r ./ng-galar-pokemons/dist/ng-galar-pokemons/* ./dist/ng-galar-pokemons
