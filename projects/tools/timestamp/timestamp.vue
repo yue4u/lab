714e4da650706e2984e5d91356e6fa24cf35ae2c
yue4u <github@yue.coffee>
2022-08-14 17:11:03 +0900

    tool: timestamp

---
+++ projects/tools/timestamp/timestamp.vue
@@ -0,0 +1,27 @@
+<template>
+  <p class="len">Date: {{ date }}</p>
+  <input type="number" v-model="text" />
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+const text = ref(+new Date);
+const date = computed(() => {
+  try {
+    return new Date(text.value).toISOString()
+  } catch (e) {
+    return e
+  }
+})
+</script>
+
+<style scoped>
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+</style>
