c3efd4c5d360d74c6d4e78b17e9171ac58eb0347
yue4u <github@yue.coffee>
2022-09-12 01:14:38 +0900

    tweak: add some description for pentachip

--- projects/games/pentachip/index.ts
+++ projects/games/pentachip/index.ts
@@ -16,9 +16,25 @@ export let ctx: CanvasRenderingContext2D;
 
 export const script: Script = {
   onMount(el) {
-    canvas = document.createElement("canvas");
+    const h = <T extends keyof HTMLElementTagNameMap>(
+      tagName: T,
+      options?: Partial<HTMLElementTagNameMap[T]>,
+    ) => Object.assign(document.createElement(tagName), options);
+
+    const title = h("h1", {
+      innerText: "Pentachip",
+    });
+    const link = h("a", {
+      innerText: "wikipedia(Chinese)",
+      rel: "noopener noreferrer",
+      href: "https://zh.wikipedia.org/zh/%E6%8C%91%E5%A4%BE%E6%A3%8B",
+    });
+    link.style.marginBottom = "1rem";
+
+    canvas = h("canvas");
     ctx = canvas.getContext("2d")!;
-    el.appendChild(canvas);
+    el.append(title, link, canvas);
+
     const game = new Pentachip(canvas);
     const players: PlayerIndex[] = ["P1", "P2"];
     game.start(game.choose(players));
