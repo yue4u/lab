450e0c4e3cccfaeb7a3edac1e95e13bd7c40335d
yue4u <github@yue.coffee>
2022-11-06 01:29:53 +0900

    fix: lazy import puzzles

--- projects/demos/8-puzzle/8-puzzle.vue
+++ projects/demos/8-puzzle/8-puzzle.vue
@@ -5,35 +5,38 @@
             rel="noopener noreferrer">https://coursera.cs.princeton.edu/algs4/assignments/8puzzle/specification.php</a>
     </p>
     <div class="input">
-        <select class="test-container" @change="
-            // @ts-expect-error
-            board = $event.target.value
-        ">
-            <option :disabled="board === val" :key="key" :value="val" v-for="[key, val] in tests">{{ key }}</option>
+        <select class="test-container" v-model="currentPuzzle">
+            <option :disabled="currentPuzzle === key" :key="key" :value="key" v-for="key in puzzles">{{ key }}
+            </option>
         </select>
         <textarea v-model="board" :rows="3"></textarea>
     </div>
     <p class="error" v-if="!solver.solvable">not solvable</p>
     <div class="steps" v-if="solver.solvable">
         <div v-for="(board, i) of solver.solved?.solution()" :key="board?.id">
-            <Board :data="board?.data" />
+            <Board v-if="board?.data" :data="board.data" />
             <p class="index">{{ i }} )</p>
         </div>
     </div>
 </template>
+
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import Board from './board.vue'
 import puzzle5 from './8puzzle/puzzle05.txt?raw'
 import { Solver } from './solver'
 
-const tests = (Object.entries(import.meta.glob('./8puzzle/puzzle*.txt', { as: 'raw' })) as any as [string, string][]).sort()
-
+const puzzleSource = import.meta.glob('./8puzzle/puzzle*.txt', { as: 'raw' })
+const puzzles = Object.keys(puzzleSource).sort()
+const currentPuzzle = ref(puzzles[5])
 const board = ref(puzzle5)
 const solver = computed(() => {
     return Solver.fromPuzzle(board.value, { maxSearchIteration: 5000 })
 });
 
+watch(currentPuzzle, async () => {
+    board.value = await puzzleSource[currentPuzzle.value]()
+})
 </script>
 
 <style scoped>
