9072d0c44a193266406304017efdd271913fc592
yue4u <github@yue.coffee>
2023-03-14 23:10:42 +0900

    feat: play with at layer

---
+++ projects/demos/at-layer/at-layer.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="root">
+    <h1><span class="at-layer revert-layer">@</span><span class="at-layer">layer</span> Experiments</h1>
+
+    <p class="text">
+      text
+    </p>
+    <p class="text revert-color">
+      text revert
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+@import "at-layer.css" layer(at-layer);
+
+@layer site,
+demo,
+this;
+
+.text {
+  color: skyblue;
+}
+
+.revert-color {
+  color: revert-layer;
+}
+
+@layer site {
+  .text {
+    font-size: 1.4rem;
+    color: aquamarine;
+  }
+
+  .title {
+    color: aquamarine;
+  }
+}
+
+@layer demo {
+  .text {
+    color: teal;
+  }
+}
+
+@layer component {
+  .text {
+    color: hotpink;
+  }
+}
+
+@layer {
+  .text {
+    color: wheat;
+  }
+
+  .revert {
+    color: lightgreen;
+  }
+
+  @layer {
+    .text {
+      color: lawngreen;
+    }
+
+  }
+}
+</style>
---
+++ projects/demos/at-layer/index.ts
@@ -0,0 +1,4 @@
+import { withVue } from "@/site/with-vue";
+import App from "./at-layer.vue";
+
+export default withVue(App);
