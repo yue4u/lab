561d457c413a4186b6bb1229781e96b1a0321aba
yue4u <github@yue.coffee>
2023-05-28 21:55:20 +0900

    feat: Make output lines responsive

--- projects/demos/webcontainer/container/index.js
+++ projects/demos/webcontainer/container/index.js
@@ -4,7 +4,24 @@ const app = express();
 const port = 3111;
 
 app.get('/', (req, res) => {
-    res.send('<p style="color: #fff">Welcome to a WebContainers app! 🥳</p>');
+    res.send(`
+<head>
+  <style>
+  body {
+      display: grid;
+      place-items: center;
+  }
+  p {
+      font-size: 2rem;
+      color: #fff;
+      text-align: center;
+  }
+  </style>
+</head>
+<body>
+  <p>Welcome to a WebContainers app! 🥳</p>
+</body>
+`);
 });
 
 app.listen(port, () => {
--- projects/demos/webcontainer/index.ts
+++ projects/demos/webcontainer/index.ts
@@ -1,27 +1,47 @@
 import { html, css, Script } from "@/site/core";
 import { FileNode, FileSystemTree, WebContainer } from '@webcontainer/api';
-import * as monaco from "monaco-editor-core";
-import EditorWorker from "monaco-editor-core/esm/vs/editor/editor.worker?worker";
+import * as monaco from "monaco-editor";
 import { Terminal } from 'xterm'
+
+self.MonacoEnvironment = {
+  getWorker(workerId: string, label: string) {
+    const getWorkerModule = (moduleUrl: string, label: string) => {
+      return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
+        name: label,
+        type: 'module'
+      });
+    };
+
+    switch (label) {
+      case 'json':
+        return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
+      case 'css':
+      case 'scss':
+      case 'less':
+        return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
+      case 'html':
+      case 'handlebars':
+      case 'razor':
+        return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
+      case 'typescript':
+      case 'javascript':
+        return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
+      default:
+        return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
+    }
+  }
+};
+
 
 if (!crossOriginIsolated) {
   location.reload()
 }
 
-self.MonacoEnvironment = {
-  getWorker() {
-    return new EditorWorker();
-  },
-};
-
-export const template = () => html`<div class="container">
-<div class="editor">
-</div>
-<div class="preview">
-  <iframe></iframe>
-</div>
-<div class="term">
-</div>
+export const template = () => html`
+<div class="container">
+  <div class="preview"><iframe></iframe></div>
+  <div class="editor"></div>
+  <div class="term"></div>
 </div>`;
 
 export const script: Script = {
@@ -36,7 +56,9 @@ export const script: Script = {
     const webcontainerInstance = await WebContainer.boot();
     await webcontainerInstance.mount(files);
 
-    const term = new Terminal();
+    const term = new Terminal({
+      convertEol: true,
+    });
     term.open(termEl);
 
     const editor = monaco.editor.create(editorEl, {
@@ -51,19 +73,23 @@ export const script: Script = {
       await webcontainerInstance.fs.writeFile('/index.js', editor.getValue());
     });
 
-    const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+    const shellProcess = await webcontainerInstance.spawn('jsh', {
+      terminal: {
+        cols: term.cols,
+        rows: term.rows,
+      }
+    });
 
-    installProcess.output.pipeTo(new WritableStream({
+    shellProcess.output.pipeTo(new WritableStream({
       write(data) {
         term.write(data);
       }
     }))
 
-    if (await installProcess.exit !== 0) {
-      alert('Installation failed');
-    };
-
-    await webcontainerInstance.spawn('npm', ['run', 'start']);
+    const input = shellProcess.input.getWriter();
+    term.onData((data) => {
+      input.write(data);
+    });
 
     webcontainerInstance.on('server-ready', (port, url) => {
       iframeEl.src = url;
@@ -74,7 +100,8 @@ export const script: Script = {
 export const style = css`
 .container {
   display: grid;
-  grid-template: 1fr;
+  grid-template-rows: 30vh;
+  grid-template-columns: 100%;
   gap: 1rem;
   height: 100%;
   width: 100%;
@@ -90,14 +117,17 @@ textarea {
   padding: 0.5rem 1rem;
 }
 
-textarea.xterm-helper-textarea{
+.xterm-helper-textarea, .xterm-helpers{
+  opacity: 0;
+}
+.xterm-scroll-area{
   display: none;
 }
-
 iframe {
   height: 100%;
   width: 100%;
   border-radius: 0.5rem;
+  border: 1px solid skyblue;
   color: #fff;
 }
 `;
