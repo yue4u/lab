e6bd8f9b214c52e63f2b9d3c55c72560ec9ae5d4
yue4u <github@yue.coffee>
2025-11-12 09:06:57 +0900

    feat: add wasm source

---
+++ projects/tools/iter-tpl/wasm/iter-tpl/iter-tpl.mjs
@@ -0,0 +1,8 @@
+import { combinations, permutations } from 'iter-tpl:iter/tools@0.0.1';
+import { execute } from 'iter-tpl:tpl/template@0.0.1';
+
+export const iterTpl = {
+    combinations,
+    permutations,
+    template: execute
+};
